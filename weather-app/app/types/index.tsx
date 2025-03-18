f53202ba26ecffa3e76bfc240adad289495a5608
Juan Pablo Gaviria <juanpablomullerg@gmail.com>
2025-03-18 18:04:34 -0500

    Added necessary folders and defining initial interfaces

---
+++ weather-app/app/types/index.tsx
@@ -0,0 +1,10 @@
+export interface WeatherData {
+    id: string;
+    city: string;
+    temperature: number;
+    description: string;
+    humidity: number;
+    windSpeed: number;
+    icon: string;
+    timestamp: number;
+  }

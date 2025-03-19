fd0ab24eb4f4549f8362e3d1efa7516e647750f0
Juan Pablo Gaviria <juanpablomullerg@gmail.com>
2025-03-18 20:56:44 -0500

    TS Type interfaces 'WeatherData' and 'ErrorState have been readded'

---
+++ weather-app-clean/app/types/index.ts
@@ -0,0 +1,15 @@
+export interface WeatherData {
+    id: string;
+    city: string;
+    temperature: number;
+    description: string;
+    humidity: number;
+    windSpeed: number;
+    icon: string;
+    timestamp: number;
+  }
+  
+  export interface ErrorState {
+    isError: boolean;
+    message: string;
+  }

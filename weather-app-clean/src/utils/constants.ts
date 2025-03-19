6064ceed753fea78594fb8b15395c1ea79556811
Juan Pablo Gaviria <juanpablomullerg@gmail.com>
2025-03-18 23:23:36 -0500

    Deleted non working folder

--- weather-app-clean/src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-import { OPENWEATHER_API_KEY } from '@env';
-
-export const API_KEY = OPENWEATHER_API_KEY || ''; // Leer desde variables de entorno
-export const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-export const STORAGE_KEY = '@weather_history';
